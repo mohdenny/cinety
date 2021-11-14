fcd73c68f6f261d7734880a55710c73311f8b4ce
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-15 01:06:53 +0700

    create comp dashboard

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <>
+            <p className="text-3xl">Dashboard</p>
+        </>
+    )
+}
+
+export default Dashboard
