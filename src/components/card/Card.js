9eb4122a1ec1025841c6c8e20c0546f79b4b546a
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-22 05:30:58 +0700

    create comp card

---
+++ src/components/card/Card.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div id="card-body" className="border-4 border-red-400 h-96">
+            <div id="card-thumbnail" className="border-2 h-2/3">
+                test
+            </div>
+            <div id="card-content" className="border-2 h-1/3">
+                <div id="card-title" className="border-2 h-1/3">
+                    test
+                </div>
+                <div id="card-desc" className="border-2 h-2/3">
+                    test
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
