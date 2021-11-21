d543d4841aa1872f5c166925db6e8ddee399f7ed
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-22 05:31:17 +0700

    import heading comp

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Heading from '../heading/Heading'
+import Card from '../card/Card'
 
 const Dashboard = () => {
     return (
         <>
-            <p className="text-3xl">Dashboard</p>
+            <Heading text="Dashboard" />
+
+            <div className="grid grid-cols-4 gap-4">
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
         </>
     )
 }
