f51cdb89d3907b3d656f30dcba0c144ea5accefc
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-22 05:32:04 +0700

    create heading comp

---
+++ src/components/heading/Heading.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Heading = ({ text }) => {
+    return <div className="border-2 py-2 text-center font-bold text-3xl mb-4"><p>{text}</p></div>
+}
+
+export default Heading
