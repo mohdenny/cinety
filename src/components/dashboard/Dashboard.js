6108b89c2fd946330da6996f4c72af30268bacb1
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-22 16:24:10 +0700

    import comp

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import Heading from '../heading/Heading'
+import Chip from '../chip/Chip'
 import Card from '../card/Card'
 
 const Dashboard = () => {
     return (
-        <>
-            <Heading text="Dashboard" />
+        <>  
+            <div className="flex flex-col space-y-4">
+                <Heading text="Dashboard" />
 
-            <div className="grid grid-cols-4 gap-4">
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
+                <div className="flex flex-row space-x-4 border-2 py-2 items-center justify-center">
+                    <Chip text="Popular"/>
+                    <Chip text="Now Playing"/>
+                    <Chip text="Upcoming"/>
+                    <Chip text="Trending"/>
+                </div>
+
+                <div className="grid grid-cols-4 gap-4">
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                </div>
             </div>
         </>
     )
