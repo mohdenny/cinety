09120de7d02db5b5b84b805543f792f92aadf06c
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-23 16:46:18 +0700

    import redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import './assets/styles/tailwind.css';
-import Sidebar from './components/layout/Sidebar';
-import Landing from './components/layout/Landing';
-import Dashboard from './components/dashboard/Dashboard';
+import React from 'react'
+import './assets/styles/tailwind.css'
+import Sidebar from './components/layout/Sidebar'
+import Landing from './components/layout/Landing'
+import Dashboard from './components/dashboard/Dashboard'
 
-function App() {
+// redux
+import { Provider } from "react-redux"
+import store from "./store"
+
+const App = () => {
   return (
-    <>
-      <Sidebar>
-        <Dashboard/>
-      </Sidebar>
-    </>
+    <Provider store={store}>
+      <>
+        <Sidebar>
+          <Dashboard/>
+        </Sidebar>
+      </>
+    </Provider>
   );
 }
 
