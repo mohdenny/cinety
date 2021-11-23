873c61464df867a0df71ba660459f2a9fceac6ef
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-23 16:46:54 +0700

    import config and add param props

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,17 +1,16 @@
 import React from 'react'
+import { IMAGE_URL } from '../../utils/config'
 
-const Card = () => {
+const Card = ({ image, title, overview }) => {
     return (
-        <div id="card-body" className="border-4 border-red-400 h-96">
-            <div id="card-thumbnail" className="border-2 h-2/3">
-                test
-            </div>
-            <div id="card-content" className="border-2 h-1/3">
-                <div id="card-title" className="border-2 h-1/3">
-                    test
+        <div id="card-body" className="h-60">
+            <img id="card-thumbnail" alt={title} className="object-fill object-center h-1/2 w-full" src={`${IMAGE_URL}${image}`} />
+            <div id="card-content" className="border-2 h-1/2 overflow-ellipsis overflow-hidden">
+                <div id="card-title" className="border-2 h-auto">
+                    <p className="text-center font-bold py-1 px-2">{title}</p>
                 </div>
-                <div id="card-desc" className="border-2 h-2/3">
-                    test
+                <div id="card-desc" className="h-auto">
+                    <p className="text-sm text-justify py-1 px-2">{overview}</p>
                 </div>
             </div>
         </div>
