ccc34c6be1872b7b2bcb6bd32139591a78c1b29c
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-23 16:47:52 +0700

    change elment attribute

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -7,15 +7,13 @@ const Navbar = ({ children }) => {
         setToggleMenu(!toggleMenu)
     }
 
-    console.log(toggleMenu)
-
     return (
         <>
             <nav className="px-8 py-2 flex flex-row items-center justify-between h-12 w-screen">
                 <div className="flex flex-row space-x-4 h-full">
                     <button className="h-full" onClick={handleToggle}>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                         </svg>
                     </button>
                     <div className="flex items-center justify-center w-full h-full">
