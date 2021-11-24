16b60a688c76284eac28a79ebc724d4d54161f53
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-24 07:49:58 +0700

    import actions top rated

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,8 +1,7 @@
 import React, { Fragment, useEffect } from "react"
 import PropTypes from "prop-types"
 import { connect } from "react-redux"
-import { getPopular, getNowPlaying, getUpcoming } from "../../actions/movies"
-import { getTrendingAll } from "../../actions/trending"
+import { getPopular, getNowPlaying, getUpcoming, getTopRated } from "../../actions/movies"
 import Heading from "../heading/Heading"
 import Chip from "../chip/Chip"
 import Card from "../card/Card"
@@ -11,30 +10,29 @@ const Dashboard = ({
     getPopular, 
     getNowPlaying, 
     getUpcoming, 
-    getTrendingAll, 
-    movies: { movies }, 
-    trending: { trending }
+    getTopRated,
+    movies: { movies }
 }) => {
     const movie = movies.data
     const clicked = movies.clicked
 
     useEffect(() => {
-        getPopular()
+        getPopular('popular')
     }, [getPopular])
 
     return (
         <>  
             <div className="flex flex-col space-y-4">
-                <Heading text="Dashboard" />
+                {/* <Heading text="Dashboard" /> */}
 
-                <div className="flex flex-row space-x-4 border-t-2 border-b-2 py-2 items-center justify-center">
+                <div className="flex flex-row space-x-4 bg-white border-b-2 py-2 items-center justify-center">
                     <Chip text="Popular" action={getPopular} name="popular" clicked={clicked}/>
                     <Chip text="Now Playing" action={getNowPlaying} name="now-playing" clicked={clicked}/>
                     <Chip text="Upcoming" action={getUpcoming} name="upcoming" clicked={clicked}/>
-                    <Chip text="Trending" action={getTrendingAll} name="trending" clicked={clicked}/>
+                    <Chip text="Top Rated" action={getTopRated} name="top-rated" clicked={clicked}/>
                 </div>
 
-                <div className="grid grid-cols-4 gap-4">
+                <div className="px-4 grid grid-cols-4 gap-4">
                     {
                         movie && movie.map(item => {
                             return (
@@ -54,7 +52,7 @@ Dashboard.propTypes = {
     getPopular: PropTypes.func.isRequired,
     getNowPlaying: PropTypes.func.isRequired,
     getUpcoming: PropTypes.func.isRequired,
-    getTrendingAll: PropTypes.func.isRequired,
+    getTopRated: PropTypes.func.isRequired,
     movies: PropTypes.object.isRequired,
     trending: PropTypes.object.isRequired
 }
@@ -68,7 +66,7 @@ export default connect(mapStateToProps, {
     getPopular, 
     getNowPlaying, 
     getUpcoming, 
-    getTrendingAll 
+    getTopRated 
 })(
     Dashboard
 )
