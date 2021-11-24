4dc6f92cd69b7c2ad7ec8554e5e0cac7b04a856c
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-24 07:11:41 +0700

    import classnames

--- src/components/chip/Chip.js
+++ src/components/chip/Chip.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import classNames from 'classnames'
 
-const Chip = ({ text }) => {
+const Chip = ({ name, clicked, action, text }) => {
+
     return (
-        <button className="border-2 border-gray-500 px-4 py-1 rounded-xl">
+        <button 
+            className={classNames("border-2 border-gray-500 px-4 py-1 rounded-xl hover:bg-gray-200 hover:text-gray-900", {
+                "bg-gray-400 text-white": name === clicked
+            })} 
+            onClick={() => action(name)}
+        >
             {text}
         </button>
     )
