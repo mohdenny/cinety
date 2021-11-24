6f0c7e4c2392b780b91ee9da6fedb0952d1c7e1a
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-24 07:11:17 +0700

    create lazy component

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import { IMAGE_URL } from '../../utils/config'
+import React, { Suspense, lazy} from 'react'
+import classNames from 'classnames'
 
-const Card = ({ image, title, overview }) => {
+const LazyComponent = lazy(() => import('./Image'))
+
+const Card = ({ imageUrl, title, overview, rating }) => {
     return (
-        <div id="card-body" className="h-60">
-            <img id="card-thumbnail" alt={title} className="object-fill object-center h-1/2 w-full" src={`${IMAGE_URL}${image}`} />
-            <div id="card-content" className="border-2 h-1/2 overflow-ellipsis overflow-hidden">
-                <div id="card-title" className="border-2 h-auto">
+        <div id="card-body" className="h-48">
+            <Suspense fallback={<p className="text-center">Loading...</p>}>
+                    <LazyComponent url={imageUrl} alt={title} rating={rating} />
+            </Suspense>
+            <div id="card-content" className="w-full h-auto">
+                <div id="card-title" className="w-full h-auto">
                     <p className="text-center font-bold py-1 px-2">{title}</p>
                 </div>
-                <div id="card-desc" className="h-auto">
-                    <p className="text-sm text-justify py-1 px-2">{overview}</p>
+                <div id="card-text" className="h-auto w-full">
+                    <p className="text-sm text-justify py-1 px-2 truncate">{overview}</p>
                 </div>
             </div>
         </div>
