ed16a934e5946313f04635d8ed633c2900e9dbd9
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-24 07:49:44 +0700

    add classnames hover

--- src/components/chip/Chip.js
+++ src/components/chip/Chip.js
@@ -2,11 +2,10 @@ import React from 'react'
 import classNames from 'classnames'
 
 const Chip = ({ name, clicked, action, text }) => {
-
     return (
         <button 
-            className={classNames("border-2 border-gray-500 px-4 py-1 rounded-xl hover:bg-gray-200 hover:text-gray-900", {
-                "bg-gray-400 text-white": name === clicked
+            className={classNames("border-2 border-gray-300 bg-gray-200 px-4 py-1 rounded-xl hover:bg-gray-300", {
+                "bg-gray-900 text-white hover:bg-gray-900 hover:text-white": name === clicked
             })} 
             onClick={() => action(name)}
         >
