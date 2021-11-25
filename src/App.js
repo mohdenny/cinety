bc35f16d135b45c24a000d6395e7e7b8a24082a4
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-25 18:14:30 +0700

    import react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react'
-import './assets/styles/tailwind.css'
-import Sidebar from './components/layout/Sidebar'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Landing from './components/layout/Landing'
-import Dashboard from './components/dashboard/Dashboard'
+import Routes from './components/routing/Routes'
 
 // redux
 import { Provider } from "react-redux"
 import store from "./store"
 
+import './assets/styles/tailwind.css'
+
 const App = () => {
   return (
     <Provider store={store}>
-      <>
-        <Sidebar>
-          <Dashboard/>
-        </Sidebar>
-      </>
+      <Router>
+        <>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+        </>
+      </Router>
     </Provider>
-  );
+  )
 }
 
 export default App;
